3bd77eb78d9ccc9ec038aec080bfbb4f884ebd1f
Saray <saraymunera0@gmail.com>
2025-10-15 15:20:27 -0500

    01-10-25

--- notas/src/App.js
+++ notas/src/App.js
@@ -1,38 +1,19 @@
-import './App.css';
-import Lista from './Lista';
-import Footer from './Footer';
-import Header from './Header';
-import Inicio from './Inicio';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Item3 from './Item3';
+import React from 'react';
+import './Lista.css';
 
-
-function App() {
-  let estudiantes = [
-    { nombre: "Sarai", apellido: "Munera" },
-    { nombre: "Alexis", apellido: "Aguirre" },
-    { nombre: "Antonieta", apellido: "Perez" },
-    { nombre: "Ana", apellido: "Gómez" },
-    { nombre: "Manuela", apellido: "Rendón" },
-    { nombre: "Miguel", apellido: "Villa" },
-    { nombre: "Daniela", apellido: "Torres" },
-    { nombre: "Thomas", apellido: "Usuga" }
-  ];
+function Lista({ listaestudiantes }) {
   return (
-    <BrowserRouter>
-      <div className='App'>
-        <Header />
-        <Routes>
-          <Route path='/' element={<Inicio />}></Route>
-          <Route path='/estudiantes'
-            element={<Lista Lista listaestudiantes={estudiantes} />} />
-        </Routes>
-
-        <Footer />
-      </div>
-    </BrowserRouter>
+    <div className="lista-container">
+      <h2>Lista de Estudiantes</h2>
+      <ul>
+        {listaestudiantes.map((est, index) => (
+          <li key={index}>
+            {est.nombre} {est.apellido}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
-
 }
 
-export default App;
+export default Lista;
--- notas/src/Header.js
+++ notas/src/Header.js
@@ -3,20 +3,31 @@ import { Link } from 'react-router-dom';
 
 function Header() {
     return (
-        <header className="header">
-            Menu
-            <nav>
-                <ul>
+        <header className='header'>
+            LOGO <hr />
+            MENU
+            <nav className='header'>
+                <ul className='header'>
                     <li>
                         <Link to="/">Inicio</Link>
                     </li>
+
                     <li>
                         <Link to="/estudiantes">Estudiantes</Link>
                     </li>
+                    <li>
+                        <Link to="/modulos">Modulos</Link>
+                    </li>
+                    <li>
+                        <Link to="/login">Iniciar sesión</Link>
+                    </li>
+                    <li>
+                        <Link to="/signin">Registrarse</Link>
+                    </li>
                 </ul>
-            </nav>  
+            </nav>
         </header>
-    );
+    )
 }
 
 export default Header;
